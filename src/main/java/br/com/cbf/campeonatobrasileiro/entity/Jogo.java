b44901ef0b76ea33fa9c52387acc6339af41cbf9
Vitor Cunha <vitorsilvarosa1902@gmail.com>
2025-08-07 20:45:19 -0300

    Classes criadas no pacote entity, Jogo e Time

--- src/main/java/br/com/cbf/campeonatobrasileiro/entity/Jogo.java
+++ src/main/java/br/com/cbf/campeonatobrasileiro/entity/Jogo.java
@@ -1,6 +1,9 @@
 package br.com.cbf.campeonatobrasileiro.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
@@ -8,9 +11,13 @@ import lombok.Data;
 
 public class Jogo {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
     private Time time1;
     private Time time2;
     private Integer golsTime1;
     private Integer golsTime2;
     private Integer publicoPagante;
+    private String uuid;
 }
